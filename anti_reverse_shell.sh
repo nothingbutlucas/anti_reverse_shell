dbc9bc48c0eb0e4832b0c10d0c1217886dc55f15
Lucas <69118979+nothingbutlucas@users.noreply.github.com>
2022-10-17 15:37:45 -0300

    It's a matter of taste...

--- anti_reverse_shell.sh
+++ anti_reverse_shell.sh
@@ -11,14 +11,14 @@
 #   * * * * * ~/.local/bin/anti_reverse_shell.sh
 
 if [[ $( ps aux | grep "bash -i" | wc -l ) -gt 1 ]]; then
-    notify-send "[ ! ] Possible reverse shell look at these processes: " "$(ps aux | grep "bash -i" | awk '{print $1, $2, $9, $11, $12, $13, $14}')" -u critical
+  kill -9 $(ps aux | grep "bash -i" | awk '{print $2}')
+  notify-send "[ ! ] I killed the possible reverse shell" -u critical
 fi
 
 # Or you can do something like
 
 # if [[ $( ps aux | grep "bash -i" | wc -l ) -gt 1 ]]; then
-#   kill -9 $(ps aux | grep "bash -i" | awk '{print $2}')
-#   notify-send "[ ! ] I killed the possible reverse shell" -u critical
+#     notify-send "[ ! ] Possible reverse shell look at these processes: " "$(ps aux | grep "bash -i" | awk '{print $1, $2, $9, $11, $12, $13, $14}')" -u critical
 # fi
 
 exit 0
